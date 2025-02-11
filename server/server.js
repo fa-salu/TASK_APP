7f8552e604bac705fdb24c94d612b06642b63473
fa-salu <fasalurahman7515@gmail.com>
2025-02-11 20:57:58 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
